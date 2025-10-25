88e205fae67c6be36f3551cd133ccae0466f61bb
Trevor Gross <tmgross@umich.edu>
2025-10-24 21:07:56 -0400

    Start using bash in verify-build.sh

--- ci/verify-build.sh
+++ ci/verify-build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Checks that libc builds properly for all supported targets on a particular
 # Rust version:
@@ -118,147 +118,146 @@ test_target() {
     fi
 }
 
-freebsd_versions="\
-11 \
-12 \
-13 \
-14 \
-15 \
-"
+freebsd_versions=(
+    11
+    12
+    13
+    14
+    15
+)
 
-rust_linux_targets="\
-aarch64-linux-android \
-aarch64-unknown-linux-gnu \
-aarch64-unknown-linux-musl \
-arm-linux-androideabi \
-arm-unknown-linux-gnueabi \
-arm-unknown-linux-gnueabihf \
-arm-unknown-linux-musleabi \
-arm-unknown-linux-musleabihf \
-armv7-linux-androideabi \
-armv7-unknown-linux-gnueabihf \
-armv7-unknown-linux-musleabihf \
-i586-unknown-linux-gnu \
-i586-unknown-linux-musl \
-i686-linux-android \
-i686-unknown-freebsd \
-i686-unknown-linux-gnu \
-i686-unknown-linux-musl \
-powerpc-unknown-linux-gnu \
-powerpc64-unknown-linux-gnu \
-powerpc64le-unknown-linux-gnu \
-s390x-unknown-linux-gnu \
-sparc64-unknown-linux-gnu \
-sparcv9-sun-solaris \
-wasm32-unknown-emscripten \
-wasm32-unknown-unknown \
-wasm32-wasip1 \
-wasm32-wasip2 \
-x86_64-linux-android \
-x86_64-unknown-freebsd \
-x86_64-unknown-linux-gnu \
-x86_64-unknown-linux-musl \
-x86_64-unknown-netbsd \
-"
+rust_linux_targets=(
+    aarch64-linux-android
+    aarch64-unknown-linux-gnu
+    aarch64-unknown-linux-musl
+    arm-linux-androideabi
+    arm-unknown-linux-gnueabi
+    arm-unknown-linux-gnueabihf
+    arm-unknown-linux-musleabi
+    arm-unknown-linux-musleabihf
+    armv7-linux-androideabi
+    armv7-unknown-linux-gnueabihf
+    armv7-unknown-linux-musleabihf
+    i586-unknown-linux-gnu
+    i586-unknown-linux-musl
+    i686-linux-android
+    i686-unknown-freebsd
+    i686-unknown-linux-gnu
+    i686-unknown-linux-musl
+    powerpc-unknown-linux-gnu
+    powerpc64-unknown-linux-gnu
+    powerpc64le-unknown-linux-gnu
+    s390x-unknown-linux-gnu
+    sparc64-unknown-linux-gnu
+    sparcv9-sun-solaris
+    wasm32-unknown-emscripten
+    wasm32-unknown-unknown
+    wasm32-wasip1
+    wasm32-wasip2
+    x86_64-linux-android
+    x86_64-unknown-freebsd
+    x86_64-unknown-linux-gnu
+    x86_64-unknown-linux-musl
+    x86_64-unknown-netbsd
+)
 
 # FIXME(powerpc64le): powerpc64le-unknown-linux-musl is tier 2 since 1.85 and
 # can be moved to rust_linux_targets once MSRV is increased
-rust_nightly_linux_targets="\
-aarch64-unknown-fuchsia \
-armv5te-unknown-linux-gnueabi \
-armv5te-unknown-linux-musleabi \
-i686-pc-windows-gnu \
-powerpc64le-unknown-linux-musl \
-riscv64gc-unknown-linux-gnu \
-x86_64-fortanix-unknown-sgx \
-x86_64-pc-solaris \
-x86_64-pc-windows-gnu \
-x86_64-unknown-fuchsia \
-x86_64-unknown-illumos \
-x86_64-unknown-linux-gnux32 \
-x86_64-unknown-redox \
-"
+rust_nightly_linux_targets=(
+    aarch64-unknown-fuchsia
+    armv5te-unknown-linux-gnueabi
+    armv5te-unknown-linux-musleabi
+    i686-pc-windows-gnu
+    powerpc64le-unknown-linux-musl
+    riscv64gc-unknown-linux-gnu
+    x86_64-fortanix-unknown-sgx
+    x86_64-pc-solaris
+    x86_64-pc-windows-gnu
+    x86_64-unknown-fuchsia
+    x86_64-unknown-illumos
+    x86_64-unknown-linux-gnux32
+    x86_64-unknown-redox
+)
 
-rust_apple_targets="\
-aarch64-apple-darwin \
-aarch64-apple-ios \
-"
+rust_apple_targets=(
+    aarch64-apple-darwin
+    aarch64-apple-ios
+)
 
-rust_nightly_apple_targets="\
-"
+
+rust_nightly_apple_targets=()
 
 # Must start with `x86_64-pc-windows-msvc` first.
-rust_nightly_windows_targets="\
-x86_64-pc-windows-msvc \
-x86_64-pc-windows-gnu \
-i686-pc-windows-msvc \
-"
+rust_nightly_windows_targets=(
+    x86_64-pc-windows-msvc
+    x86_64-pc-windows-gnu
+    i686-pc-windows-msvc
+)
 
 # Targets which are not available via rustup and must be built with -Zbuild-std
 # FIXME(hexagon): hexagon-unknown-linux-musl should be tested but currently has
 # duplicate symbol errors from `compiler_builtins`.
-rust_linux_no_dist_targets="\
-aarch64-pc-windows-msvc \
-aarch64-unknown-freebsd \
-aarch64-unknown-hermit \
-aarch64-unknown-netbsd \
-aarch64-unknown-openbsd \
-aarch64-wrs-vxworks \
-armebv7r-none-eabi \
-armebv7r-none-eabihf \
-armv7-wrs-vxworks-eabihf \
-armv7r-none-eabi \
-armv7r-none-eabihf \
-i686-pc-windows-msvc \
-i686-unknown-haiku \
-i686-unknown-netbsd \
-i686-unknown-openbsd \
-i686-wrs-vxworks \
-mips-unknown-linux-gnu \
-mips-unknown-linux-musl \
-mips64-unknown-linux-gnuabi64 \
-mips64-unknown-linux-muslabi64 \
-mips64el-unknown-linux-gnuabi64 \
-mips64el-unknown-linux-muslabi64 \
-mipsel-unknown-linux-gnu \
-mipsel-unknown-linux-musl \
-nvptx64-nvidia-cuda \
-powerpc-unknown-linux-gnuspe \
-powerpc-unknown-netbsd \
-powerpc-wrs-vxworks \
-powerpc-wrs-vxworks-spe \
-powerpc64-unknown-freebsd \
-powerpc64-wrs-vxworks \
-riscv32i-unknown-none-elf \
-riscv32imac-unknown-none-elf \
-riscv32imc-unknown-none-elf \
-riscv32gc-unknown-linux-gnu \
-riscv32-wrs-vxworks \
-riscv64gc-unknown-freebsd \
-riscv64gc-unknown-hermit \
-riscv64gc-unknown-linux-musl \
-riscv64gc-unknown-none-elf \
-riscv64imac-unknown-none-elf \
-riscv64-wrs-vxworks \
-s390x-unknown-linux-musl \
-sparc-unknown-linux-gnu \
-sparc64-unknown-netbsd \
+rust_linux_no_dist_target=(
+    aarch64-unknown-freebsd
+    aarch64-unknown-hermit
+    aarch64-unknown-netbsd
+    aarch64-unknown-openbsd
+    aarch64-wrs-vxworks
+    armebv7r-none-eabi
+    armebv7r-none-eabihf
+    armv7-wrs-vxworks-eabihf
+    armv7r-none-eabi
+    armv7r-none-eabihf
+    i686-pc-windows-msvc
+    i686-unknown-haiku
+    i686-unknown-netbsd
+    i686-unknown-openbsd
+    i686-wrs-vxworks
+    mips-unknown-linux-gnu
+    mips-unknown-linux-musl
+    mips64-unknown-linux-gnuabi64
+    mips64-unknown-linux-muslabi64
+    mips64el-unknown-linux-gnuabi64
+    mips64el-unknown-linux-muslabi64
+    mipsel-unknown-linux-gnu
+    mipsel-unknown-linux-musl
+    nvptx64-nvidia-cuda
+    powerpc-unknown-linux-gnuspe
+    powerpc-unknown-netbsd
+    powerpc-wrs-vxworks
+    powerpc-wrs-vxworks-spe
+    powerpc64-unknown-freebsd
+    powerpc64-wrs-vxworks
+    riscv32i-unknown-none-elf
+    riscv32imac-unknown-none-elf
+    riscv32imc-unknown-none-elf
+    riscv32gc-unknown-linux-gnu
+    riscv32-wrs-vxworks
+    riscv64gc-unknown-freebsd
+    riscv64gc-unknown-hermit
+    riscv64gc-unknown-linux-musl
+    riscv64gc-unknown-none-elf
+    riscv64imac-unknown-none-elf
+    riscv64-wrs-vxworks
+    s390x-unknown-linux-musl
+    sparc-unknown-linux-gnu
+    sparc64-unknown-netbsd
 
-thumbv6m-none-eabi \
-thumbv7em-none-eabi \
-thumbv7em-none-eabihf \
-thumbv7m-none-eabi \
-thumbv7neon-linux-androideabi \
-thumbv7neon-unknown-linux-gnueabihf \
-thumbv8m.main-none-eabi \
-x86_64-pc-windows-msvc \
-x86_64-unknown-dragonfly \
-x86_64-unknown-haiku \
-x86_64-unknown-hermit \
-x86_64-unknown-l4re-uclibc \
-x86_64-unknown-openbsd \
-x86_64-wrs-vxworks \
-"
+    thumbv6m-none-eabi
+    thumbv7em-none-eabi
+    thumbv7em-none-eabihf
+    thumbv7m-none-eabi
+    thumbv7neon-linux-androideabi
+    thumbv7neon-unknown-linux-gnueabihf
+    thumbv8m.main-none-eabi
+    x86_64-pc-windows-msvc
+    x86_64-unknown-dragonfly
+    x86_64-unknown-haiku
+    x86_64-unknown-hermit
+    x86_64-unknown-l4re-uclibc
+    x86_64-unknown-openbsd
+    x86_64-wrs-vxworks
+)
 
 rust_apple_no_dist_targets="\
 armv7s-apple-ios \
@@ -276,7 +275,9 @@ elif [ "$os" = "macos" ]; then
     nightly_targets="$rust_nightly_apple_targets"
     no_dist_targets="$rust_apple_no_dist_targets"
 elif [ "$os" = "windows" ]; then
-    targets=${rust_nightly_windows_targets}
+    targets="$rust_windows_targets"
+    nightly_targets="$rust_nightly_windows_targets"
+    no_dist_targets=""
 else
     exit 1
 fi
